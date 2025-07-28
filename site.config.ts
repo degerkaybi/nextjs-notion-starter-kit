3a261169a19f2b43bdc4183839a9dd8ebcf2d593
degerkaybi <degerkaybi@gmail.com>
2025-07-28 16:50:47 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -27,7 +27,7 @@ export default siteConfig({
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+  defaultPageCoverPosition: 6.5,
 
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: false,
