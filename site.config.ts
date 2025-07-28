400bf061b39a4bfa8fc0a575d8602fc8dd7f031e
degerkaybi <degerkaybi@gmail.com>
2025-07-28 14:27:43 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,3 +1,5 @@
+import { siteConfig } from './lib/site-config'
+
 export default siteConfig({
   rootNotionPageId: '7479b6483c7c47d1a4b16ec3c3b15060',
 
@@ -10,7 +12,7 @@ export default siteConfig({
   description: '...........',
 
   twitter: 'x',
-  instagram: 'ig',
+  instagram: 'i',
   giphy: 'g',
 
   defaultPageIcon: null,
@@ -20,35 +22,32 @@ export default siteConfig({
   isPreviewImageSupportEnabled: true,
   isRedisEnabled: false,
 
+  pageUrlOverrides: {
+    '/silent-steps': '62d2c83cd0e0465490db610ddb78bfc7',
+    '/silen-steps-night-watch-milan-ed': '22a392488fe5803fa915f0b8c5bd2ebe',
+    '/silen-steps-panda': '22a392488fe580c8aae6fcdebd717fb8',
+    '/silent-steps-figures-edition': '22a392488fe58064b772d74978226a72',
+    '/night-watch': 'bd62c75cff0b4b11a8571ea394db1d59',
+    '/night-watch-bull-run-edition': '4902f670013a466182341cc6d94f00d4',
+    '/rabbit-h0le': '22a392488fe580778278ccc0674023b1',
+    '/dreams': 'f4ec07636c974df688d64ff6631ec26c',
+    '/mamut-art': '22a392488fe580f28c33e898a8fc01a6',
+    '/eid-la': 'da5cf4ffa5a74d81b2b4651d85c9a9d8',
+    '/faces': '22a392488fe580508c6fd28fd81cb677'
+  },
 
-
-pageUrlOverrides: {
-  '/silent-steps': '62d2c83cd0e0465490db610ddb78bfc7',
-  '/silen-steps-night-watch-milan-ed': '22a392488fe5803fa915f0b8c5bd2ebe',
-  '/silen-steps-panda': '22a392488fe580c8aae6fcdebd717fb8',
-  '/silent-steps-figures-edition': '22a392488fe58064b772d74978226a72',
-  '/night-watch': 'bd62c75cff0b4b11a8571ea394db1d59',
-  '/night-watch-bull-run-edition': '4902f670013a466182341cc6d94f00d4',
-  '/rabbit-h0le': '22a392488fe580778278ccc0674023b1',
-  '/dreams': 'f4ec07636c974df688d64ff6631ec26c',
-  '/mamut-art': '22a392488fe580f28c33e898a8fc01a6',
-  '/eid-la': 'da5cf4ffa5a74d81b2b4651d85c9a9d8',
-  '/faces': '22a392488fe580508c6fd28fd81cb677'
-},
-
-navigationStyle: 'custom',
-navigationLinks: [
-  { title: 'Silent Steps', pageId: '62d2c83cd0e0465490db610ddb78bfc7' },
-  { title: 'Silen Steps Night Watch Milan Ed', pageId: '22a392488fe5803fa915f0b8c5bd2ebe' },
-  { title: 'Silen Steps Panda', pageId: '22a392488fe580c8aae6fcdebd717fb8' },
-  { title: 'Silent Steps Figures Edition', pageId: '22a392488fe58064b772d74978226a72' },
-  { title: 'Night Watch', pageId: 'bd62c75cff0b4b11a8571ea394db1d59' },
-  { title: 'Night Watch Bull Run Edition', pageId: '4902f670013a466182341cc6d94f00d4' },
-  { title: 'Rabbit H0le', pageId: '22a392488fe580778278ccc0674023b1' },
-  { title: 'Dreams', pageId: 'f4ec07636c974df688d64ff6631ec26c' },
-  { title: 'Mamut Art', pageId: '22a392488fe580f28c33e898a8fc01a6' },
-  { title: 'Eid La', pageId: 'da5cf4ffa5a74d81b2b4651d85c9a9d8' },
-  { title: 'Faces', pageId: '22a392488fe580508c6fd28fd81cb677' }
-]
-
+  navigationStyle: 'custom',
+  navigationLinks: [
+    { title: 'Silent Steps', pageId: '62d2c83cd0e0465490db610ddb78bfc7' },
+    { title: 'Silen Steps Night Watch Milan Ed', pageId: '22a392488fe5803fa915f0b8c5bd2ebe' },
+    { title: 'Silen Steps Panda', pageId: '22a392488fe580c8aae6fcdebd717fb8' },
+    { title: 'Silent Steps Figures Edition', pageId: '22a392488fe58064b772d74978226a72' },
+    { title: 'Night Watch', pageId: 'bd62c75cff0b4b11a8571ea394db1d59' },
+    { title: 'Night Watch Bull Run Edition', pageId: '4902f670013a466182341cc6d94f00d4' },
+    { title: 'Rabbit H0le', pageId: '22a392488fe580778278ccc0674023b1' },
+    { title: 'Dreams', pageId: 'f4ec07636c974df688d64ff6631ec26c' },
+    { title: 'Mamut Art', pageId: '22a392488fe580f28c33e898a8fc01a6' },
+    { title: 'Eid La', pageId: 'da5cf4ffa5a74d81b2b4651d85c9a9d8' },
+    { title: 'Faces', pageId: '22a392488fe580508c6fd28fd81cb677' }
+  ]
 })
