a5bcf6f87b667ae19c219daf7e352393c203d55f
degerkaybi <degerkaybi@gmail.com>
2025-07-28 17:15:18 +0300

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -3,20 +3,20 @@ import styles from './styles.module.css'
 export function GitHubShareButton() {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://kaybid.live'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
       aria-label='View source on GitHub'
     >
       <svg
-        width='80'
-        height='80'
+        width='0'
+        height='0'
         viewBox='0 0 250 250'
         style={{
           fill: '#70B7FD',
           color: '#fff',
-          position: 'absolute',
+
           zIndex: 1001,
           top: 0,
           right: 0,
