35b376842fd892887465f9223fed1c56eac9dfd9
Yigit Duman <yigitq@gmail.com>
2025-09-02 16:48:16 +0300

    fix build

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -27,7 +27,7 @@ import { GitHubShareButton } from './GitHubShareButton'
 import { Loading } from './Loading'
 import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
-import { PageAside } from './PageAside'
+// import { PageAside } from './PageAside'
 import { PageHead } from './PageHead'
 import styles from './styles.module.css'
 
@@ -234,16 +234,16 @@ export function NotionPage({
   const showTableOfContents = !!isBlogPost
   const minTableOfContentsItems = 3
 
-  const pageAside = React.useMemo(
-    () => (
-      <PageAside
-        block={block!}
-        recordMap={recordMap!}
-        isBlogPost={isBlogPost}
-      />
-    ),
-    [block, recordMap, isBlogPost]
-  )
+  // const pageAside = React.useMemo(
+  //   () => (
+  //     <PageAside
+  //       block={block!}
+  //       recordMap={recordMap!}
+  //       isBlogPost={isBlogPost}
+  //     />
+  //   ),
+  //   [block, recordMap, isBlogPost]
+  // )
 
   const footer = React.useMemo(() => <Footer />, [])
 
