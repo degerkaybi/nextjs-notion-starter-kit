a05220dc0f445881a6b6fe8f3c13044996e87bab
degerkaybi <degerkaybi@gmail.com>
2026-02-08 22:08:17 +0300

    Update pageUrlOverrides in site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -55,7 +55,6 @@ instagram: 'kaybid', // Örnek: 'kaybidsteps'
         //  pageUrlOverrides: null,
   
 pageUrlOverrides: {
- 
   '/resimlerimin-mirildandiklari': '1d1392488fe580ea834ae3c6a21590a7',
   '/kavramsal-metin': '1cf392488fe58017954aea45ed6ee5e1',
   '/in-between-sips': '1bf392488fe5801eb9d8cdd52696975c',
